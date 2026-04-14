f75055d93964fa0023ddbdc83eb55c1bc0bfeff0
richard.su <sekzerd@outlook.com>
2026-04-14 18:54:00 +0800

    fix: fix ts error

--- src/renderer/src/router.ts
+++ src/renderer/src/router.ts
@@ -1,10 +1,7 @@
 import { createMemoryHistory, createRouter } from "vue-router"
 
-import HomePage from "@renderer/pages/Home.vue"
 const routes = [
     { path: "/", name: "HomePage", component: () => import("@renderer/pages/Home.vue") },
-
-
 ]
 
 export const router = createRouter({
